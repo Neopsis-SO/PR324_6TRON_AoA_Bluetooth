2382b03bc8631ad50ba05276e48ea30d80b63d81
Neopsis-SO <72496337+Neopsis-SO@users.noreply.github.com>
2022-01-23 20:34:34 +0100

    Initialization_and_echo

--- main.cpp
+++ main.cpp
@@ -7,8 +7,13 @@
 
 
 // Blinking rate in milliseconds
-#define BLINKING_RATE     100ms
+#define BLINKING_RATE     500ms
+#define UART_SLEEP  1000ms
+#define BAUD_RATE   9600
 
+BufferedSerial serial_port (UART1_TX, UART1_RX);
+char buffer[256] = "First send with UART\n";
+int bufferSize = 0;
 
 int main()
 {
@@ -19,8 +24,27 @@ int main()
     bool led;
 #endif
 
+    // Initialise the serial communication
+    serial_port.set_baud(BAUD_RATE);
+    serial_port.set_format(
+        /* bits */ 8,
+        /* parity */ BufferedSerial::None,
+        /* stop bit */ 1
+    );
+
+    printf("Lauched !\n");
+    serial_port.write(buffer, sizeof(buffer));
+
     while (true) {
-        led = !led;
-        ThisThread::sleep_for(BLINKING_RATE);
+        
+        serial_port.write(buffer, sizeof(buffer));
+        if (uint32_t bufferSize = serial_port.read(buffer, sizeof(buffer))) {
+            // Toggle the LED.
+            led.write(!led);
+
+            // Echo the input back to the terminal.
+            serial_port.write(buffer, bufferSize);
+        }
+        ThisThread::sleep_for(UART_SLEEP);
     }
 }
