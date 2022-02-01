575d272296a516db0c7b6778d6529e66424a099c
Neopsis-SO <72496337+Neopsis-SO@users.noreply.github.com>
2022-02-01 09:05:53 +0100

    Read UART correction first character

--- src/UART/UART.cpp
+++ src/UART/UART.cpp
@@ -16,16 +16,16 @@ osStatus UART_TaskInit (void)
 
 // static UART_print()
 // {
-//     char buffer[64];
-//     xUARTreceiveQueue.try_get(buffer);
-//     printf("%s\n", buffer);
+//     char bufferSend[64];
+//     xUARTreceiveQueue.try_get(bufferSend);
+//     printf("%s\n", bufferSend);
 // }
 
 
 static char special_char = ',';
 
 // Extract data from a frame
-void extract_data(char *trame, struct UBLOX_Data *Data)
+void extract_data(char *trame, UBLOX_Data *Data)
 {
     static char rssi_pol1[NB_CHAR_SECT] = "0\0";
     static char angle_azimuth[NB_CHAR_SECT] = "0\n";
@@ -43,6 +43,10 @@ void extract_data(char *trame, struct UBLOX_Data *Data)
     for (int i = 0;  p != '\0' ; i++)
     { 
         p = trame[i];
+        if(j == 4)
+        {
+            break;
+        }
         // Comparaison to find delimiter character
         if (p == special_char)
         {
@@ -69,24 +73,26 @@ void extract_data(char *trame, struct UBLOX_Data *Data)
                     rssi_pol2[k_4] = trame[i];
                     k_4++;
                     break;
+                default :
+                    break;
             }
         }
     }
     // Conversion and value assignment at the struct
-    if(j >= 5)
+    if(j >= 4)
     {
-        Data->rssi_pol1 = atoi(rssi_pol1);
-        Data->angle_azimuth = atoi(angle_azimuth);
-        Data->angle_elevation = atoi(angle_elevation);
-        Data->rssi_pol2 = atoi(rssi_pol2);
+        // Data->rssi_pol1 = atoi(rssi_pol1);
+        // Data->angle_azimuth = atoi(angle_azimuth);
+        // Data->angle_elevation = atoi(angle_elevation);
+        // Data->rssi_pol2 = atoi(rssi_pol2);
     }
 }
 
 void UART_Task()
 {
-    char buffer[128] = "AT";
-    char buffer2[128];
-
+    char bufferSend[UART_BUFFER_SIZE] = "\0";
+    char bufferReceived[UART_BUFFER_SIZE] = "\0";
+    UBLOX_Data receivedUBLOXdata;
 
     // Initialise the digital pin LED1 as an output
     #ifdef LED1
@@ -95,38 +101,36 @@ void UART_Task()
         bool led;
     #endif
     ThisThread::sleep_for(2000ms);
-    serial_port.write(buffer, sizeof(buffer));
-    printf("%s\n",buffer);
+    serial_port.write(bufferSend, sizeof(bufferSend));
+    printf("%s\n",bufferSend);
     // ThisThread::sleep_for(1000ms);
-    // serial_port.read(buffer, sizeof(buffer));
-    // printf("%s", buffer);
+    // serial_port.read(bufferSend, sizeof(bufferSend));
+    // printf("%s", bufferSend);
 
 
     while(true)
     {
-        // char command[8], id[16];
-        // int rssi, azimut, elevation, rssi2 = 0;
-        struct UBLOX_Data received_data;
         char c;
         int i = 0;
         if(serial_port.readable())
         {
             while(serial_port.read(&c, 1))
             {
-                strcat(buffer2, &c);
+                strcat(bufferReceived, &c);
                 i++;
                 if(!strcmp(&c, "\n"))
                 {
                     led.write(!led);
-                    buffer2[i-1] = '\0';
-                    printf("%s\n", buffer2);
-                    extract_data(buffer2, &received_data);
-                    // sscanf(buffer2, "%*:%s,%d,%d,%d,%d,%*", id, &rssi, &azimut, &elevation, &rssi2);
+                    bufferReceived[i-1] = '\0';
+                    printf("%s\n", bufferReceived);
+                    extract_data(bufferReceived, &receivedUBLOXdata);
+                    // sscanf(bufferReceived, "%*:%s,%d,%d,%d,%d,%*", id, &rssi, &azimut, &elevation, &rssi2);
                     // printf("%d, %d, %d, %d\n", rssi, azimut, elevation, rssi2);
-                    // printf("%d, %d, %d, %d\n", received_data.rssi_pol1, received_data.angle_azimuth, received_data.angle_elevation, received_data.rssi_pol2);
+                    // printf("%d, %d, %d, %d\n", receivedUBLOXdata.rssi_pol1, receivedUBLOXdata.angle_azimuth, receivedUBLOXdata.angle_elevation, receivedUBLOXdata.rssi_pol2);
+                    bufferReceived[0] = '\0';
                     break;
                 }
-                // printf("%c", buffer2[i-1]); //With this line, the software is ok for slow and medium speed UBLOX communication
+                // printf("%c", bufferReceived[i-1]); //With this line, the software is ok for slow and medium speed UBLOX communication
             }
             // printf("i=%d\n", i);  // #warning : never go to this line because serial_port.read() never return 0, find why and a solution to find when all data has been received !
         } else {
--- src/UART/UART.h
+++ src/UART/UART.h
@@ -1,13 +1,15 @@
-#include "6TRON_AoA_Bluetooth.h"
-
 #ifndef __UART_H__
 #define __UART_H__
+
+#include "6TRON_AoA_Bluetooth.h"
 
 #define UART_TASK_PRIORITY      osPriorityNormal
 #define UART_TASK_STACK_SIZE    OS_STACK_SIZE
 
 #define UART_SLEEP_TASK  100ms
 #define UART_BAUD_RATE   115200
+
+#define UART_BUFFER_SIZE    256
 
 #define NB_CHAR_SECT 32
 #define NB_CHAR_TRAME 256
@@ -16,12 +18,11 @@ void UART_Task();
 osStatus UART_TaskInit (void);
 
 // Store data collect from UBLOX-C211
-struct UBLOX_Data
-{
+typedef struct {
     int rssi_pol1;
     int angle_azimuth;
     int angle_elevation;
     int rssi_pol2;
-};
+} UBLOX_Data;
 
 #endif
