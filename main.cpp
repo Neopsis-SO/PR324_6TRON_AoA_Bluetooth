39e22d87a010b594097b286f43d28b3024fbb3f0
Neopsis-SO <72496337+Neopsis-SO@users.noreply.github.com>
2022-02-02 16:52:23 +0100

    Test de queue

--- main.cpp
+++ main.cpp
@@ -5,22 +5,48 @@
 
 #include "mbed.h"
 
+typedef struct {
+    int    voltage;   /* AD result of measured voltage */
+    int    current;   /* AD result of measured current */
+    uint32_t counter;   /* A counter value               */
+} message_t;
 
-// Blinking rate in milliseconds
-#define BLINKING_RATE     100ms
+MemoryPool<message_t, 16> mpool;
+rtos::Queue<message_t, 16> queue;
+Thread thread;
 
-
-int main()
+/* Send Thread */
+void send_thread(void)
 {
-    // Initialise the digital pin LED1 as an output
-#ifdef LED1
-    DigitalOut led(LED1);
-#else
-    bool led;
-#endif
+    uint32_t i = 0;
+    while (true) {
+        i++; // fake data update
+        message_t *message = mpool.try_alloc();
+        message->voltage = i + 1;
+        message->current = i + 2;
+        message->counter = i;
+        if(queue.try_put(message) == true)
+        //     printf("Message OK\n");
+        ThisThread::sleep_for(1s);
+    }
+}
+
+int main(void)
+{
+    thread.start(callback(send_thread));
 
     while (true) {
-        led = !led;
-        ThisThread::sleep_for(BLINKING_RATE);
+        osEvent evt = queue.get();
+        if (evt.status == osEventMessage) {
+            message_t *message = (message_t *)evt.value.p;
+            // message_t ** message;
+            // if(queue.try_get(&message) == true) {
+                printf("\nVoltage: %d V\n\r", message->voltage);
+                printf("Current: %d A\n\r", message->current);
+                printf("Number of cycles: %lu\n\r", message->counter);
+
+                mpool.free(message);
+            }
+        // }
     }
 }
